ffb6f2061ead533fe1fc8e8ed9974fc99da70c15
QaziPython <rafehqazi1@gmail.com>
2016-04-30 02:04:41 -0500

    Create heroku database.

--- tournament/database.py
+++ tournament/database.py
@@ -1,8 +1,10 @@
 from . import app
 import psycopg2
+import os
 
 
-conn = psycopg2.connect('dbname=tournament')
+db_url = os.environ.get("DATABASE_URL", "dbname=tournament")
+conn = psycopg2.connect(db_url)
 def hello_sql():
     with conn, conn.cursor() as cur:
         cur.execute('select * from players')
