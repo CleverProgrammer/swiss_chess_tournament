af4c1a4e63a8ebb9d61a10c9cc9b53c6b611b1be
rafeh01 <rafehqazi1@gmail.com>
2016-04-30 06:32:28 -0500

    Added count players query and some extra css.

--- tournament/views.py
+++ tournament/views.py
@@ -6,19 +6,27 @@ from . import database
 @app.route("/")
 def hello():
     # players = database.hello_sql()
-    return render_template('index.html',
-                           matches=database.all_matches(), players=database.hello_sql())
+    return render_template('index.html', matches=database.all_matches(),
+                           players=database.hello_sql())
     # return render_template("index.html", players=database.hello_sql())
+
 
 @app.route("/addplayer", methods=["GET"])
 def addplayer_get():
     return render_template("add_player.html")
 
+
 @app.route("/addplayer", methods=["POST"])
 def addplayer_post():
-    winner = request.form["winner_id"]
-    loser = request.form["loser_id"]
-    database.reportMatch(winner, loser)
-    # database.registerPlayer(player)
+    player = request.form['player']
+    # winner = request.form["winner_id"]
+    # loser = request.form["loser_id"]
+    # database.reportMatch(winner, loser)
+    database.registerPlayer(player)
     return redirect(url_for("hello"))
 
+
+@app.route('/query', methods=['POST'])
+def query_result():
+    # query = request.form['Total Player Count']
+    return render_template('query.html', query=database.countPlayers())
