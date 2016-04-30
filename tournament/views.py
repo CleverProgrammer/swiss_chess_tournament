41b58fe88bc61dab466583ebff7fedc3c7fa355b
QaziPython <rafehqazi1@gmail.com>
2016-04-30 01:40:58 -0500

    Add CSS styling and templates.

--- tournament/views.py
+++ tournament/views.py
@@ -1,8 +1,10 @@
+from flask import render_template
+
 from . import app
 from . import database
 
 
 @app.route("/")
 def hello():
-    players = database.hello_sql()
-    return str(players)
+    # players = database.hello_sql()
+    return render_template("index.html", players=database.hello_sql())
